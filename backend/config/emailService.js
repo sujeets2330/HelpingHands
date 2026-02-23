33217d1d67ee1273997fc148247eaaaafa9bf2ab
sujeet <sujeetmalagundi999@gmail.com>
2026-02-23 10:54:32 +0530

    mail service

--- backend/config/emailService.js
+++ backend/config/emailService.js
@@ -29,56 +29,23 @@
 
 // module.exports = sendEmail;
 
-// const sgMail = require("@sendgrid/mail");
-
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const sendEmail = async ({ to, subject, text }) => {
-//   try {
-//     await sgMail.send({
-//       // from: process.env.EMAIL_USER,
-//       from: "Helpinghands@example.com", 
-//       to,
-//       subject,
-//       text,
-//     });
-
-//     console.log(" Email sent via SendGrid");
-//   } catch (error) {
-//     console.error(" SendGrid error:", error.response?.body || error);
-//   }
-// };
-
-// module.exports = sendEmail;
-
 const sgMail = require("@sendgrid/mail");
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async ({
-  from,
-  to,
-  subject,
-  text,
-  html,
-  replyTo,
-}) => {
+const sendEmail = async ({ to, subject, text }) => {
   try {
     await sgMail.send({
-      from,        // must be verified in SendGrid
+      // from: process.env.EMAIL_USER,
+      from: "Helpinghands@example.com", 
       to,
       subject,
       text,
-      html,
-      replyTo,
     });
 
-    console.log("Email sent via SendGrid");
+    console.log(" Email sent via SendGrid");
   } catch (error) {
-    console.error(
-      "SendGrid error:",
-      error.response?.body || error
-    );
+    console.error(" SendGrid error:", error.response?.body || error);
   }
 };
 
